5a38d1238b3db58a16226cbf15d94f55ae0c03af
Snehel <sahilrps31@gmail.com>
2021-08-13 12:32:02 +0530

    modified some vectors content

--- Vectors.cpp
+++ Vectors.cpp
@@ -40,6 +40,17 @@ int main()
     }
     cout << endl;
 
+    vector<int> v1 = {3, 7, 1, 8, 2, 9, 2, 5};
+    // sorted vector will be -> {1,2,2,3,5,7,8,9}
+
+    nth_element(v1.begin(), v1.begin() + 3, v1.end());
+    // this sorts the vector in such a way that all the elements lesser than 3rd index value(from the sorted vector) will be at left side of the vector and all the elements greater than 3rd index value(from the sorted vector) will be at right side of the vector
+
+    for (auto i : v1)
+    {
+        cout << i << " ";
+    }
+
     //To find the index(integer values) of an iterator
     auto i4 = find(v.begin(), v.end(), 4);
     int index = distance(v.begin(), i4);
