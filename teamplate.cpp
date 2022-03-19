a3c4f1649e56f197384b09e15fa79956d98b6e35
Snehel <sahilrps31@gmail.com>
2022-03-19 18:10:23 +0530

    added CP template

---
+++ teamplate.cpp
@@ -0,0 +1,39 @@
+// my cpp template for competitive programming contests
+// i will keep updating it with new macros and more data structure template structs as soon as i learn new stuffs
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+#define nl "\n"
+#define deb(x) cout << #x << " " << x << " ";
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define f(i, a, n) for (int i = a; i < n; i++)
+
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
